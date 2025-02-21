2e919beedc03c8d34e61f72e1a1c9a1f64bbe08b
Fedor Nikiforov <nick.g.omsk@gmail.com>
2025-02-21 16:45:18 +0600

    commenting dropDownManufactWeightPriceJS complete

--- script/dropDownManufactWeightPrice.js
+++ script/dropDownManufactWeightPrice.js
@@ -2,7 +2,7 @@ const arrowSvgManufactWeight = document.querySelectorAll('form svg');
 
 
 
-function hideFilter(e) {
+function hideFilter(e) {//rotate Svg 
     if(e.currentTarget.classList.contains('rotateSvg')){
         e.currentTarget.classList.remove('rotateSvg')
     }else{
@@ -10,13 +10,13 @@ function hideFilter(e) {
     };
 
 
-   if(e.currentTarget.previousElementSibling.classList.contains('openFilter')){
+   if(e.currentTarget.previousElementSibling.classList.contains('openFilter')){//form -> <fieldset id="manufacturer"> -> <div class="wrapperForHidden openFilter">
         e.currentTarget.previousElementSibling.classList.remove('openFilter')
    }else{
         e.currentTarget.previousElementSibling.classList.add('openFilter')
    };
     
-    e.currentTarget.previousElementSibling.querySelectorAll('div').forEach( (item) =>{
+    e.currentTarget.previousElementSibling.querySelectorAll('div').forEach( (item) =>{//<div class="wrapperForHidden openFilter"> --> all div(with input, label, span)
         if(item.classList.contains('openFilterItem')){
             item.classList.remove('openFilterItem')
         }else{
@@ -26,6 +26,6 @@ function hideFilter(e) {
     
 };
 
-arrowSvgManufactWeight.forEach( (item) =>{
+arrowSvgManufactWeight.forEach( (item) =>{//starts hideFilter on click svg
     item.addEventListener('click', hideFilter)
 } );
