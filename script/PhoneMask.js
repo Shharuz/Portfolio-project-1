0460b8bc680afba363c757e860b1756e4dc29889
Fedor Nikiforov <nick.g.omsk@gmail.com>
2025-02-22 18:57:32 +0600

    commenting PhoneMask.js complete

--- script/PhoneMask.js
+++ script/PhoneMask.js
@@ -3,17 +3,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     let phoneInputs = document.querySelectorAll('input[data-tel-input]');
 
-    let getInputNumbersValue = function(input) {
+    let getInputNumbersValue = function(input) { //prohibition on entering all symbols except numbers
         return input.value.replace(/\D/g, "")
     }
 
     let onPhoneInput = function(e) {
-        let input = e.target,
-            inputNumbersValue = getInputNumbersValue(input);
+        let input = e.target,                               //элемент инпут
+            inputNumbersValue = getInputNumbersValue(input);//хранятся только числа
         formattedInputValue = "";
-        selectionStart = input.selectionStart;
+        selectionStart = input.selectionStart;//I don't know why
 
-        if (!inputNumbersValue) {
+        /*if (!inputNumbersValue) {
             return input.value = "";
         };
 
@@ -23,52 +23,52 @@ document.addEventListener('DOMContentLoaded', () => {
                 input.value = inputNumbersValue;
             }
             return;
-        }
+        }*/
 
 
         if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
-            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;//if the first character is 9 then replace it with 7 9
 
-            let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";//if the first character is 8 then it will return 8 otherwise it will return +7
             formattedInputValue = firstSymbols + " ";
-            if (inputNumbersValue.length > 1) {
-                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            if (inputNumbersValue.length > 1) { //if more than 1 character is entered, 
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);//it will add '(' + characters from 2nd to 5th
             }
-            if (inputNumbersValue.length >= 5) {
-                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            if (inputNumbersValue.length >= 5) {//if 5 or more characters are entered
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);//it will add ') ' + characters from 5nd to 8th
             }
-            if (inputNumbersValue.length >= 8) {
-                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            if (inputNumbersValue.length >= 8) {//if 8 or more characters are entered
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);//it will add '-' + characters from 8nd to 10th
             }
-            if (inputNumbersValue.length >= 10) {
-                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            if (inputNumbersValue.length >= 10) {//if 10 or more characters are entered
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);//it will add '-' + characters from 10nd to 12th
             }
 
 
         } else {
 
-            formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+            formattedInputValue = "+" + inputNumbersValue.substring(0, 16);//it will add '+' + characters from 1st to 12th
 
         };
         input.value = formattedInputValue;
     };
 
     let onPhoneInputKeyDown = function(e) {
-        console.log(e.keyCode, e.target.value);
+        console.log(e.keyCode, e.target.value);//key code and input value (   100 - code         '+7 (984) 56' - input value    )
         let input = e.target;
-        if (e.keyCode == 8 && getInputNumbersValue(input).length == 1) {
+        if (e.keyCode == 8 && getInputNumbersValue(input).length == 1) {//if backspace is entered(keyCode == 8), the input value will be replaced with ''
             input.value = '';
         }
     }
 
     let onPhonePaste = function(e) {
-        let pasted = e.clipboardData || window.clipboardData;
-        input = e.target;
-        inputNumbersValue = getInputNumbersValue(input);
+        let pasted = e.clipboardData || window.clipboardData;//the copied number is stored
+        input = e.target;//input stored
+        inputNumbersValue = getInputNumbersValue(input);//the input is checked to make sure there are only numbers
 
-        if (pasted) {
-            let pastedText = pasted.getData('text');
-            if (/\D/g.test(pastedText)) {
+        if (pasted) {                                //if there is any data when inserting
+            let pastedText = pasted.getData('text');//inserts a value as a string from the copied text
+            if (/\D/g.test(pastedText)) {           //checks that there are only numbers
                 input.value = inputNumbersValue;
             }
         }
@@ -76,9 +76,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (i = 0; i < phoneInputs.length; ++i) {
         let input = phoneInputs[i];
-        input.addEventListener('input', onPhoneInput);
-        input.addEventListener('keydown', onPhoneInputKeyDown);
-        input.addEventListener('paste', onPhonePaste);
+        input.addEventListener('input', onPhoneInput);//on line 10
+        input.addEventListener('keydown', onPhoneInputKeyDown);//on line 56
+        input.addEventListener('paste', onPhonePaste);//on line 64
     };
 
 
