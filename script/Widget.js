c5c4f2c3b3f3d74582aee6d2bd65135a638dc175
Fedor Nikiforov <nick.g.omsk@gmail.com>
2025-02-27 16:21:11 +0600

    commenting Widget.js complete

--- script/Widget.js
+++ script/Widget.js
@@ -1,18 +1,17 @@
-const trigger = document.querySelector('.widget__trigger');
 const list = document.querySelector('.widget__ul');
+const trigger = document.querySelector('.widget__trigger');
 const widget = document.querySelector('.widget__trigger__main');
 const cross = document.querySelector('.widget__trigger__cross');
 
 let openWidget = function () {
-        if( list.classList.contains('hide')){
-            list.classList.remove('hide');
-            widget.classList.add('hidetrigger');
-            cross.classList.remove('hidetrigger');
-
+        if( !list.classList.contains('visibleWidgetlist') ){//scss on line 4349
+            list.classList.add('visibleWidgetlist')//the list drops to the top
+            widget.classList.add('hidetrigger')   //changes the widget icon to a cross
+            cross.classList.remove('hidetrigger')
         }else{
-            list.classList.add('hide');
-            widget.classList.remove('hidetrigger');
-            cross.classList.add('hidetrigger');
+            list.classList.remove('visibleWidgetlist')//the list comes back
+            widget.classList.remove('hidetrigger')   
+            cross.classList.add('hidetrigger')
         }
 };
 
