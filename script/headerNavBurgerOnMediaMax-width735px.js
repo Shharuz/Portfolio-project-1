3cb30d7cd3505fdb23e55c9c8ec22f1ecae49b76
Fedor Nikiforov <nick.g.omsk@gmail.com>
2025-02-28 16:17:04 +0600

    fix validation html and header list

--- script/headerNavBurgerOnMediaMax-width735px.js
+++ script/headerNavBurgerOnMediaMax-width735px.js
@@ -16,31 +16,3 @@ arrowOpenList.forEach((item) => {//when you click on the arrow a list drop down
     });
 });
 
-/*const arrowOpenList = document.querySelectorAll('.arrow-rigth-wrapper');
-const headerNavMainList = document.querySelector('.header__nav__main-list');
-console.log(arrowOpenList);
-
-
-arrowOpenList.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (item.classList.contains('for-go-to-catalog')) {
-            if (!headerNavMainList.classList.contains('move-down-nav__main-list')) {
-                headerNavMainList.classList.add('move-down-nav__main-list')
-            } else {
-                headerNavMainList.classList.remove('move-down-nav__main-list')
-            };
-            console.log(headerNavMainList);
-        }
-
-        if (!item.nextElementSibling.classList.contains('open-sub-list')) {
-            item.nextElementSibling.classList.add('open-sub-list')
-        } else {
-            item.nextElementSibling.classList.remove('open-sub-list')
-        };
-        if (!item.classList.contains('arrow-rigth-wrapper-rotate')) {
-            item.classList.add('arrow-rigth-wrapper-rotate')
-        } else {
-            item.classList.remove('arrow-rigth-wrapper-rotate')
-        };
-    });
-});*/
