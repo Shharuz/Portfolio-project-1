ca7e029eb52e9b8aa3361859fe5c0c12fbc6fbc4
Fedor Nikiforov <nick.g.omsk@gmail.com>
2025-05-09 16:58:33 +0600

    adaptive questions complete

--- script/questionsDropDown.js
+++ script/questionsDropDown.js
@@ -2,10 +2,17 @@ const svgDropDown = document.querySelectorAll('.questions__question-and-answer__
 
 svgDropDown.forEach( (item) =>{
     item.addEventListener('click', () =>{         //opens and closes the answer to the question, on adding/removing a class
+           if(!item.classList.contains('rotate-svg-questions')){
+               item.classList.add('rotate-svg-questions')
+           }else{
+               item.classList.remove('rotate-svg-questions')
+           }
+
            if(!item.nextElementSibling.classList.contains('open-question')){
                 item.nextElementSibling.classList.add('open-question')
            }else{
                 item.nextElementSibling.classList.remove('open-question')
            };
+
     } );
 } );
